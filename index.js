7d24716426f94ebd9818a93bba96bcdfdb7b98b5
Amjad <amjadmaqsuma@gmail.com>
2022-05-31 08:33:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
+
+    for (let i = 0; i < array.length; i++) {
+      const complement = target - array[i];
+      for (let j = i + 1; j < array.length; j++) {
+        if (array[j] === complement) return true;
+      }
+    }
+  
+    return false;
+  }
+
 
 /* 
   Write the Big O time complexity of your function here
-*/
-
+*/ // Runtime: O(n^2)
 /* 
   Add your pseudocode here
 */
@@ -29,6 +37,21 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+  
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([4, 2, 5], 5));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, 2, 6, 5], 7));
 }
 
 module.exports = hasTargetSum;
